d76b75a10c7c03b8cc2cc0ef4512b21b6f0d32ef
wu-kan <github@wu-kan.cn>
2025-11-13 19:22:10 +0800

    try fixing segment fault

--- ex5-pass/lib/ToyPassFuse.cpp
+++ ex5-pass/lib/ToyPassFuse.cpp
@@ -24,7 +24,7 @@ struct FusePat : mlir::OpRewritePattern<toy::AddOp> {
     auto result = rewriter.create<toy::AddOp>(
         op->getLoc(), inputs.front().getType(), mlir::ValueRange(inputs));
     rewriter.replaceOp(op1, mlir::ValueRange(result));
-    op->erase();
+    // op->erase();
     return mlir::success();
   }
 };
