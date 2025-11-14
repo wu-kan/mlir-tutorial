3ac16cf5584169ae8d79ec93f478f363b096709b
wu-kan <github@wu-kan.cn>
2025-11-14 12:14:21 +0800

    fix inline mlir

--- ex5-pass/ex5-pass.cpp
+++ ex5-pass/ex5-pass.cpp
@@ -11,21 +11,22 @@ int main(int argc, char **argv) {
   ctx.loadDialect<mlir::func::FuncDialect>();
   ctx.loadDialect<toy::ToyDialect>();
   mlir::OpBuilder builder(&ctx);
-  llvm::StringRef s = "\
-module {\
-  func.func @test(%arg0: i32, %arg1: i32) -> i32 {\
-    %0 = \"toy.sub\"(%arg0, %arg1) : (i32, i32) -> i32\
-    %1 = \"toy.add\"(%0, %arg0, %arg1) : (i32, i32, i32) -> i32\
-    %2 = \"toy.const\"() <{value = 7 : i32}> : () -> i32\
-    %3 = \"toy.add\"(%0, %1, %2) : (i32, i32, i32) -> i32\
-    %4 = \"toy.add\"(%arg0, %arg1) : (i32, i32) -> i32\
-    %5 = \"toy.add\"(%4, %arg1) : (i32, i32) -> i32\
-    %6 = \"toy.add\"(%5, %arg1) : (i32, i32) -> i32\
-    return %6 : i32\
-  }\
-}\
+  llvm::StringRef s = "                                          \n\
+module {                                                         \n\
+  func.func @test(%arg0: i32, %arg1: i32) -> i32 {               \n\
+    %0 = \"toy.sub\"(%arg0, %arg1) : (i32, i32) -> i32           \n\
+    %1 = \"toy.add\"(%0, %arg0, %arg1) : (i32, i32, i32) -> i32  \n\
+    %2 = \"toy.const\"() <{value = 7 : i32}> : () -> i32         \n\
+    %3 = \"toy.add\"(%0, %1, %2) : (i32, i32, i32) -> i32        \n\
+    %4 = \"toy.add\"(%arg0, %arg1) : (i32, i32) -> i32           \n\
+    %5 = \"toy.add\"(%4, %arg1) : (i32, i32) -> i32              \n\
+    %6 = \"toy.add\"(%5, %arg1) : (i32, i32) -> i32              \n\
+    return %6 : i32                                              \n\
+  }                                                              \n\
+}                                                                \n\
 ";
   auto mod = mlir::parseSourceString<mlir::ModuleOp>(s, &ctx);
+  mod->dump();
 
   do {
     mlir::PassManager pm(&ctx);
